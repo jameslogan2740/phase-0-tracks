0b4089c7ba2c483a5991e3eaf289c488ebf0c8ed
James Logan <james.logan@gmail.com>
2017-02-20 22:28:23 -0600

    found missing string operations.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,47 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+ swap= "iNvEsTiGaTiOn".swapcase
+ #=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+zoom="zom".insert(1,"0")
+ #=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+enhance="enhance".center(20)
+ #=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+up="Stop! You’re under arrest!".upcase
+ #=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+addition="the usual"<< " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+beggining=" suspects".insert(0,"the usual ")
+#=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+dissappear="The case of the disappearing last letter".chop
+#=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+missing="The mystery of the missing first letter".slice(1..38)
+#=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+elementary="Elementary,    my   dear        Watson!".split.join("")
+#=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+ordinal="z".ord #returns the integer ordinal of a one character string.
+#=> 122 
 
-# "How many times does the letter 'a' appear in this string?".<???>
+count="How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+
+puts "swap letters; #{swap}"
+puts "add letter to zoom; #{zoom}"
+puts "add space to #{enhance}"
+puts "uppcase everything; #{up}"
+puts "add to string; #{addition}"
+puts "add to beggining; #{beggining}"
+puts "dissapear last letter; #{dissapear}"
+puts "dissapear first letter; #{missing}"
+puts "join string w/o spaces; #{elementary}"
+puts "return integer ordinal #{ordinal}"
+puts "count the times A appear in string; #{count}"
